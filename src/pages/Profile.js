235acf5f98a99c5cbe98c5ee45b29432c7049b97
raizafr <frraiza056@gmail.com>
2023-04-14 20:48:16 +0700

    update

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,25 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Profile = () => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState([]);
   console.log(user);
-  axios
-    .get("http://api.dev.indothon.com/api/v1/auth/profile", {
-      withCredentials: true,
-    })
-    .then(function (response) {
-      // handle success
-      setUser(response.data);
-      // console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-    }, []);
+  const getUser = () => {
+    axios
+      .get("http://api.dev.indothon.com/api/v1/auth/profile", {
+        withCredentials: true,
+      })
+      .then(function (response) {
+        // handle success
+        setUser(response.data);
+        // console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  };
+  useEffect(() => getUser(), []);
   return (
     <section className="flex justify-center mt-16">
       <div className="bg-primary w-1/3 rounded">
@@ -28,10 +31,10 @@ const Profile = () => {
         </div>
         <div className="text-white text-xl text-center pb-14">
           <ul className="space-y-2">
-            {/* <li>firsname {user.first_name}</li>
+            <li>firsname {user.first_name}</li>
             <li>lastname {user.last_name} </li>
             <li>username {user.username} </li>
-            <li>email {user.email} </li> */}
+            <li>email {user.email} </li>
           </ul>
         </div>
       </div>
